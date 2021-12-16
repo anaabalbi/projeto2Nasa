d49a5988deb2973a2bbc6d209c714d5dcecdf253
anaabalbi <anaabalbi@gmail.com>
2021-12-16 19:28:03 -0300

    javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+$('.envia').click(function () {
+  let data = $('.data').val();
+  $.ajax({
+    url: `https://api.nasa.gov/planetary/apod?api_key=T2YrB10rjkiEbm1TucDDAXbtyrvvaTPXPrh0v7g0&date=${data}`,
+    type: 'GET',
+    success: function (data) {
+      console.log(data);
+      $('.foto').attr('src', data.url);
+      $('.titulo').text(data.title);
+      $('.texto').text(data.explanation);
+    },
+  });
+});
